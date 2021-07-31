1ecc799968acd225e44ad1e537354648ab980891
SeyedMohammadHoseinTabatabaei <mohammad.h_78@yahoo.com>
2021-08-01 02:34:44 +0430

    Base Template

--- Maktab_52_Django_Clothing_Store/Maktab_52_Django_Clothing_Store/urls.py
+++ Maktab_52_Django_Clothing_Store/Maktab_52_Django_Clothing_Store/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer/', include('customer.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('core/', include('core.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
 ]
--- Maktab_52_Django_Clothing_Store/core/views.py
+++ Maktab_52_Django_Clothing_Store/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views import generic
+
+
+class MyView(generic.TemplateView):
+    template_name = 'base_temp/base.html'
